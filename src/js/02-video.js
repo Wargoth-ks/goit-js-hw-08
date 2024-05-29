1aa1aa6e505cc15fd63d6c7c8eb2812c8fcda130
Wargoth-ks <warheart1986@gmail.com>
2024-05-29 08:45:37 +0300

    2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+    'timeupdate',
+    throttle(function (data) {
+        localStorage.setItem('videoplayer-current-time', data.seconds);
+    }, 600)
+);
+
+player
+    .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+    .then(function () {})
+    .catch(function (error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+            default:
+                // some other error occurred
+                break;
+        }
+    });
